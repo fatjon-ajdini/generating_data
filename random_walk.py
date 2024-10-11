8272dcb631772aa964fa2d7540b406d055258b0c
Fatjon Ajdini <fatjonajdini11@gmail.com>
2024-10-11 12:00:10 +0200

    visual

--- random_walk.py
+++ random_walk.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 class RandomWalk:
     """A class to generate random walks."""
 
@@ -25,3 +26,15 @@ class RandomWalk:
             y_direction = choice([1, -1])
             y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
+
+            # Reject moves that go nowhere.
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calculate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
+
